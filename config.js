45726154dc43a5086161432c67a8624f1133cf6b
mhannah25 <104009729+mhannah25@users.noreply.github.com>
2022-04-19 21:20:00 +0700

    Update config.js

--- config.js
+++ config.js
@@ -11,7 +11,7 @@ const CONFIG = {
     btnReply: 'Send with luv <3',
     reply: 'Đúng vậy, bạn là dễ thương nhất rồi <333 =)))))))',
     mess: 'Mình biết mà 🥰. Yêu bạn nhiều nhiều 😘😘',
-    messDesc: 'Học hành thi cử vất vả quá thì về tâm sự với mình nhé? Hôm nào đẹp trời thì mình lại kể chuyện cho bạn nghe nha?',
+    messDesc: 'Cảm ơn cậu đã xuất hiện trong cuộc đời mình, mang lại cho mình nhiều cảm xúc thiệt là thú vị. Mình mến cậu nhiều lắm, cậu chắc không hình dung nổi đâu. Học hành thi cử vất vả quá thì về tâm sự với mình nhé? Mình luôn ở phía sau lưng cậu đây này. Hôm nào đẹp trời thì mình lại kể những câu chuyện hề hước của cô bé thiếu nghị lực cho cậu nghe nha? Yêu thương cậu nhiều <3',
     btnAccept: 'Oke <3',
     messLink: 'https://www.facebook.com/messages/t/100079674971138' //link mess của các bạn. VD: https://www.facebook.com/messages/t/100014188333536
 }
